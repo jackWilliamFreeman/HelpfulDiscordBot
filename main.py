cf459ad2f87a85fc884906038100435ef52905b6
William Freeman <wfreeman@fmgl.com.au>
2021-04-18 17:46:42 +0800

    added more things

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-
+import finnhub
 from decouple import config
 import discord
 from discord.ext import commands
+import boto3
 
 token = config('TOKEN')
+stonks_bot_id= int(config('STONKS_BOT_ID'))
+finnhub_token = config("FINNHUB_TOKEN")
+
 bot = commands.Bot(command_prefix="$", case_insensitive=True)
+finnhub_client = finnhub.Client(api_key=finnhub_token)
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('last_price')
+initial_buyin = 115
 
 
+@bot.event
+async def on_ready():
+    print("I'm ready.")
+
+#command entry
 @bot.command()
 async def stonkssupportbot(ctx, *args):
     if len(args) > 0:
@@ -19,7 +32,37 @@ async def stonkssupportbot(ctx, *args):
 
 @bot.event
 async def on_message(message):
-    if(message.author.id == 550616372120387585) and message.content.lower().find('gme') != -1:
-        await message.channel.send("gotcha")
+    if(message.author.id == stonks_bot_id):
+        dem_gains = get_gains()
+        old_gains = get_last_gains()
+        save_gains(dem_gains)
+        if dem_gains > old_gains:
+            await message.channel.send("Congratulations Ian on your winning Ape Strategy, you have improved on last time!")
+        if dem_gains < old_gains:
+            await message.channel.send("Oh no Ian has lost some gains and sits at {}%!\r\n it's not too late you can get help at: https://www.lifeline.org.au/".format(dem_gains))
+        if dem_gains == old_gains:
+            await message.channel.send("Ian's gainst haven't changed since last time I checked (still at {}%).".format(dem_gains))
+
+def get_gains():
+     quote = finnhub_client.quote('GME')
+     current_price = quote["c"]
+     dem_gains = round((current_price/initial_buyin - 1) * 100,2)
+     return dem_gains
+
+def get_last_gains():
+    resp = table.get_item(
+    Key={
+        'PK':'last_gains'
+    }
+)
+    return float(resp['Item']['gains'])
+
+def save_gains(gains):
+    table.put_item(
+        Item={
+        'PK': 'last_gains',
+        'gains': str(gains),
+    }
+)
 
 bot.run(token) 
